0a25a89480d3455f04f0b6d1e87fa5a64bd16b08
SecondGithub12138 <chenzhuofupaypal@gmail.com>
2025-03-16 14:19:54 -0700

    icon & logo

--- constants/index.ts
+++ constants/index.ts
@@ -42,8 +42,8 @@ export const IdentificationTypes = [
 
 export const Doctors = [
   {
-    image: "/assets/images/dr-green.png",
-    name: "John Green",
+    image: "/assets/images/dr-dashan.png",
+    name: "Dashan Liao",
   },
   {
     image: "/assets/images/dr-cameron.png",
